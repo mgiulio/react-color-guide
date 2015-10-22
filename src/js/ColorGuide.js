77bf5e2df040e237408782e2cecd97b507529ae1
mgiulio <giulio.mainardi@gmail.com>
2015-10-22 17:02:23 +0200

    Auto color codes

--- src/js/ColorGuide.js
+++ src/js/ColorGuide.js
@@ -1,24 +1,42 @@
 var
    React = require('react')
    //,ReactDOM = require('react-dom')
+   ,tinycolor = require('tinycolor2')
    ,Clipboard = require('clipboard')
 ;
 
 var ColorGuide = React.createClass({
 
    render: function() {
-      var swatches = this.props.colors.map(s =>
-         <li className="swatch" key={s.code.substring(1)}>
-            <div className="sample" style={{backgroundColor: s.code}}></div>
-            <h2 className="name">{s.name || s.code}</h2>
-            <ul className="codes">
-               <li className="code hex" data-clipboard-text={s.code}>
-                  <span className="label">{s.code}</span>
-                  <span className="notify">Copied</span>
-               </li>
-            </ul>
-         </li>
-      );
+      var swatches = this.props.colors.map(s => {
+         var
+            c = tinycolor(s.code),
+            hex = c.toHexString(),
+            rgb = c.toRgbString(),
+            hsl = c.toHslString()
+         ;
+
+         return (
+            <li className="swatch" key={s.code.substring(1)}>
+               <div className="sample" style={{backgroundColor: s.code}}></div>
+               <h2 className="name">{s.name || s.code}</h2>
+               <ul className="codes">
+                  <li className="code hex" data-clipboard-text={hex}>
+                     <span className="label">{hex}</span>
+                     <span className="notify">Copied</span>
+                  </li>
+                  <li className="code rgb" data-clipboard-text={rgb}>
+                     <span className="label">{rgb}</span>
+                     <span className="notify">Copied</span>
+                  </li>
+                  <li className="code hsl" data-clipboard-text={hsl}>
+                     <span className="label">{hsl}</span>
+                     <span className="notify">Copied</span>
+                  </li>
+               </ul>
+            </li>
+         );
+      });
 
       return (
          <ul className="colors">
